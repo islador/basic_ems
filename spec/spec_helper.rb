f0ac5f266f071f36f29dc43dc3f9cfe07c493c29
islador <luke.isla@gmail.com>
2014-06-01 19:44:18 -0700

    Added support for Capybara to the spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  #adding support for Capybara and FactoryGirl
+  config.include Capybara::DSL
+  config.include FactoryGirl::Syntax::Methods
 end
