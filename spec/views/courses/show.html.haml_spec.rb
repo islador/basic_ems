ff1e0df95df7130d0c30f80f36ee72cb2f722d2a
islador <luke.isla@gmail.com>
2014-06-01 23:01:57 -0700

    Built out the courses show page to match the student's show page. All course show page specs are green.

--- spec/views/courses/show.html.haml_spec.rb
+++ spec/views/courses/show.html.haml_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe "courses/show.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let!(:course) {FactoryGirl.create(:course)}
+
+	subject {page}
+
+	before(:each) do
+		visit course_path(course)
+	end
+
+	it "should have the course's name" do
+		should have_selector('#course_name', text: course.name)
+	end
+
+	it "should have a button to delete the course" do
+		should have_button('Delete Course')
+	end
+
+	it "should delete the course when the delete button is pressed" do
+		click_button('Delete Course')
+		Course.where("id = ?", course.id)[0].should be_nil
+	end
 end
