9b1bc57f993534834abb7edf89e7e04b7f29ed59
islador <luke.isla@gmail.com>
2014-06-01 21:43:25 -0700

    Built out specs for the show page, built out a delete button. The specs are now green and a user may now be deleted via the show page. This completes basic student CRUD.

--- spec/views/students/show.html.haml_spec.rb
+++ spec/views/students/show.html.haml_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe "student/show.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let!(:student) {FactoryGirl.create(:student)}
+	
+	subject {page}
+
+	before(:each) do
+		visit student_path(student)
+	end
+
+	it "should have the student's name" do
+		should have_selector('#student_name', text: student.name)
+	end
+
+	it "should have a button to delete the student" do
+		should have_button('Delete Student')
+	end
+
+	it "should delete the student when the delete button is pressed" do
+		click_button('Delete Student')
+		Student.where("id = ?", student.id)[0].should be_nil
+	end
 end
