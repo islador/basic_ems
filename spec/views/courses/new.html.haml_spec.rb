c89aae87bffa87bff9b57fbaf363bb2b1df970ab
islador <luke.isla@gmail.com>
2014-06-01 23:02:48 -0700

    Built out the course's new page to match the student's new page. All course new page show specs are green. Next up is to build out the enroll and index mechanisms and pages.

--- spec/views/courses/new.html.haml_spec.rb
+++ spec/views/courses/new.html.haml_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe "courses/new.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+	subject {page}
+
+	before(:each) do
+		visit new_course_path
+	end
+
+	it "should have a form to enter a new Course's name" do
+		should have_selector('input#course_name')
+	end
+
+	it "should have a button to submit the new course" do
+		should have_selector('input#submit_new_course')
+	end
+
+	it "should create a new course when the submit_new_course button is clicked" do
+		fill_in('Name', :with => "Advanced Psych")
+		expect{
+			click_button('Create New Course')
+		}.to change(Course, :count).by(+1)
+	end
 end
