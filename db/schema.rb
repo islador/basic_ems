d8676e33ffe670d1d98a7d64a274846cfe7a7b04
islador <luke.isla@gmail.com>
2014-06-02 20:27:23 -0700

    Added a name column to the Assignments table and updated supporting specs.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602053234) do
+ActiveRecord::Schema.define(version: 20140603032536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140602053234) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "courses", force: true do |t|
